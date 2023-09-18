8367f225e5dff0c7e08401a29a41e2f27ce57d4a
Amr Malik <amr.malik@torontomu.ca>
2023-09-18 19:31:22 -0400

    added test1 test2 route handlers

--- lecture2/middleware.py
+++ lecture2/middleware.py
@@ -2,8 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/test1')
+def test1() :
+    return "Accessed endpoint test1:  powered by Flask!!!"
 
 
+@app.route('/test2')
+def test1() :
+    return "Accessed endpoint test2:  powered by Flask!!!"
 
 if __name__ == '__main__'
     app.run()
