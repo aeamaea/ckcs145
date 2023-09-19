b519759c0b34af8a356a5f6c2ab80541c3a72365
Amr Malik <amr.malik@torontomu.ca>
2023-09-18 20:31:13 -0400

    fixed more typoes

--- lecture2/middleware.py
+++ lecture2/middleware.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def rootpath() :
+    return "Root path / reached "
+
 @app.route('/test1')
 def test1() :
     return "Accessed endpoint test1:  powered by Flask!!!"
