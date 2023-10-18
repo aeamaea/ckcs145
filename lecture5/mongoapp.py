393e4b2f484c18a98d165f3a60f5bdd76afd9799
Amr Malik <amr.malik@torontomu.ca>
2023-10-18 13:57:31 -0400

    implemented /update route and related method to persist data back into the mongodb

--- lecture5/mongoapp.py
+++ lecture5/mongoapp.py
@@ -17,17 +17,73 @@ class Customer(db.Document):
 
     meta = {'collection' : 'Customer', 'allow_inheritance' : False}
 
+def customer_exists(cust_id):
+    customers = Customer.objects(customer_id=cust_id)
+    customer = customers.first()
+
+    if not customer:
+        return False
+    else:
+        return True
+
+@app.route('/create', methods=['POST'])
+def create_customer():
+    customer_id = request.form.get('customer_id')
+    name = request.form.get('name')
+    price = request.form.get('price')
+    quantity = request.form.get('quantity')
+
+    new_customer = Customer(customer_id=customer_id,name=name,price=price,quantity=quantity)
+    x = new_customer.save()
+
+    print(x.to_json())
+
+    return(new_customer.to_json())
+
+@app.route('/update', methods=["POST"])
+def update_customer():
+    form_customer_id = request.form.get('customer_id')
+    form_name = request.form.get('name')
+    form_price = request.form.get('price')
+    form_quantity = request.form.get('quantity')
+
+    # Get customer from the backend by giving the Customer object the customer_id 
+    # we got from the form
+    customers = Customer.objects(customer_id=form_customer_id)
+    customer = customers.first() # This gives the first item in the QuerySet or None
+
+    # print(form_customer_id,form_name,form_price,form_quantity)
+    # print(customer.to_json())
+
+
+    if not customer:
+        return("Customer ID NOT FOUND!")
+    else:
+        # No need to set customer_id because the customer object already has that 
+        # we'll just update the other fields and hit save()
+        customer.name=form_name
+        customer.price=form_price
+        customer.quantity=form_quantity
+        customer.save()         # this basically saves it in the mongo back end.
+        
+        
+        return(jsonify({"customer":customer.to_json()}))
+
+
+
 @app.route('/list', methods=['GET'])
 def list_customers():
     #print(db) # print out what db we're in
 
     # for customer in Customer.objects :
     #     print("\n-------\ncustomer: ", customer.name,"\ncustomer price : ",customer.price)
-    
+
+
+    customers=list(Customer.objects)
     # return "Success!"
-    return render_template('listing.html', customer_objects=list(Customer.objects))
+    return render_template('listing.html',customer_list=customers )
     #return list(Customer.objects)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
