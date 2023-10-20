e04d76a1fcda77c53307c38985f9bd0eb82c5a15
Amr Malik <amr.malik@torontomu.ca>
2023-10-19 22:28:33 -0400

    commit everything? huh?

--- lecture5/mongoapp.py
+++ lecture5/mongoapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, request, session, abort
+from flask import Flask, jsonify, request, render_template, request, session, abort,redirect
 
 from flask_mongoengine import MongoEngine
 
@@ -17,16 +17,15 @@ class Customer(db.Document):
 
     meta = {'collection' : 'Customer', 'allow_inheritance' : False}
 
-def customer_exists(cust_id):
-    customers = Customer.objects(customer_id=cust_id)
-    customer = customers.first()
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-    if not customer:
-        return False
-    else:
-        return True
+@app.route('/data_entry')
+def data_entry():
+    return render_template('data_entry.html')
 
-@app.route('/create', methods=['POST'])
+@app.route('/create', methods=['GET','POST'])
 def create_customer():
     customer_id = request.form.get('customer_id')
     name = request.form.get('name')
@@ -34,11 +33,16 @@ def create_customer():
     quantity = request.form.get('quantity')
 
     new_customer = Customer(customer_id=customer_id,name=name,price=price,quantity=quantity)
-    x = new_customer.save()
+    new_customer.save()
 
-    print(x.to_json())
+    return redirect("/list")
 
-    return(new_customer.to_json())
+@app.route('/update-form/<cust_id>', methods=["GET"])
+def update_for(cust_id):
+    customers = Customer.objects(customer_id=cust_id) # Ask Mongo if collection has a document with customer_id we're sending
+    customer = customers.first() # This gives the first item in the QuerySet or None if nothing came back from Mongodb
+
+    return render_template("update_form.html",customer=customer)
 
 @app.route('/update', methods=["POST"])
 def update_customer():
@@ -69,30 +73,31 @@ def update_customer():
         
         return(jsonify({"customer":customer.to_json()}))
 
+@app.route('/delete/<cust_id>')
+def delete_custid(cust_id):
+    customers = Customer.objects(customer_id=cust_id)
+    customer = customers.first()
+
+    if customer:
+        customer.delete()
+
+    return redirect('/list')
+
+
 @app.route('/delete', methods=["POST"])
 def delete_customer():
     form_customer_id = request.form.get('customer_id')
-    form_name = request.form.get('name')
-    form_price = request.form.get('price')
-    form_quantity = request.form.get('quantity')
 
     # Get customer from the backend by giving the Customer object the customer_id 
     # we got from the form
     customers = Customer.objects(customer_id=form_customer_id)
-    customer = customers.first() # This gives the first item in the QuerySet or None
-
-    # print(form_customer_id,form_name,form_price,form_quantity)
-    # print(customer.to_json())
-
+    customer = customers.first() # Returns first item in the QuerySet or None
 
     if not customer:
         return("Customer not found!")
     else:
-        # No need to set customer_id because the customer object already has that 
-        
+        # No need to set customer_id because the customer object already has that         
         customer.delete()         # Nuke it with a vengeance!
-        
-        
         return("Deleted Customer with id: "+form_customer_id)
 
 
