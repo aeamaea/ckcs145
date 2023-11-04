b4ca209614fd9943a66cb88f010554548f264f71
Amr Malik <amr.malik@torontomu.ca>
2023-11-04 10:31:59 -0400

    v1.0 - see tags - all core requirements fullfilled - mongo integration, jquery AJAX for inline delivery cost, responsive html tag=v1.0

--- project-wip/app.py
+++ project-wip/app.py
@@ -4,12 +4,14 @@ from flask import Flask, request, jsonify, render_template
 from dotenv import dotenv_values
 from datetime import datetime
 
+from flask_mongoengine import MongoEngine
+
 # get secrets from the .env file 
 config=dotenv_values(".env")
 
-import pugsql 
-queries = pugsql.module('queries/')
-queries.connect('postgresql://'+config["PG_USER"]+':'+config["PG_PASS"]+'@localhost:5432/jwfoods')
+# import pugsql 
+# queries = pugsql.module('queries/')
+# queries.connect('postgresql://'+config["PG_USER"]+':'+config["PG_PASS"]+'@localhost:5432/jwfoods')
 
 
 """
@@ -21,14 +23,68 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 
+app.config['MONGODB_SETTINGS']= {'db':'jwfoods', 'host':'localhost', 'port':27017}
+db = MongoEngine()
+db.init_app(app)
+
+
+class Coeffs(db.Document):
+	weight_coeff = db.FloatField()
+	distance_coeff = db.FloatField()
+
+	meta = {'collection' : 'Coeffs', 'allow_inheritance' : False}
+
+class Visits(db.Document):
+	ip_address = db.StringField()
+	timestamp = db.StringField()
+
+	meta = {'collection' : 'Visits', 'allow_inheritance' : False}
+
+class Contacts(db.Document): 
+
+	contact_name = db.StringField()
+	email = db.StringField()
+	comments = db.StringField()
+	ip_address = db.StringField()
+	timestamp = db.StringField()
+
+	meta = {'collection' : 'Contacts', 'allow_inheritance' : False}
+
+
 @app.route("/", methods=["GET"])
 def homepage():
-	visit_ip = request.remote_addr
-	visit_time = datetime.now()
-	insert_retcode = queries.insert_visit(visit_time=visit_time, visit_ip=visit_ip)
-	print("insert return code: ", insert_retcode)
+	ip_address = request.remote_addr
+	timestamp = str(datetime.now())
+	# insert_result = queries.insert_visit(visit_time=visit_time, visit_ip=visit_ip)
+	new_visit = Visits(ip_address=ip_address, timestamp=timestamp)
+	print(ip_address,timestamp,"populated obj",new_visit.ip_address,"--", new_visit.timestamp)
+	new_visit.save()
+
+	# print("insert return code: ", insert_result)
 
 	return render_template('company.html')
+
+@app.route("/calc_charges",methods=["POST"])
+def calc_charges():
+
+	distance = float(request.form["distance"])
+	weight = float(request.form["weight"])
+
+	# get the coefficients from the Coeff documents 
+	# from the Mongo backend.
+	coeffs = Coeffs.objects.first()
+	weight_coeff=coeffs.weight_coeff
+	dist_coeff=coeffs.distance_coeff
+
+	delivery_cost = round((distance * dist_coeff) + (weight * weight_coeff),2)
+	print(weight,distance,delivery_cost)
+
+	return jsonify({"delivery_cost" : delivery_cost})
+
+
+
+
+
 
 if __name__=="__main__":
 	app.run(host='0.0.0.0', port=5000, debug=True)
